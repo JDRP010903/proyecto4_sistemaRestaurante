ca2293525f588ce12fc42cf073b72cbfb6ae4422
Jorge Rea <danireaprado@hotmail.com>
2024-01-29 00:13:56 -0600

    SobreNosotrosPage y Modal login y sugerencias terminados

--- src/components/LoginModalComponent.jsx
+++ src/components/LoginModalComponent.jsx
@@ -8,15 +8,24 @@ const LoginModalComponent = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                     <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="loginModalLabel">Iniciar Sesión</h1>
+                        <h1 className="modal-title fs-5" id="loginModalLabel">Inicio de Sesión</h1>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        ...
+                        <form className="mt-3">
+                            <div className="mb-3">
+                                <label htmlFor="username" className="form-label" style={{ color: 'var(--color2)' }}>Nombre de Usuario</label>
+                                <input type="text" className="form-control" id="username" placeholder="Ingresa tu usuario" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label" style={{ color: 'var(--color2)' }}>Contraseña:</label>
+                                <input type="password" className="form-control" id="password" placeholder="Ingresa tu contraseña" />
+                            </div>
+                        </form>
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button" className="btn btn-primary"  data-bs-dismiss="modal">Iniciar Sesión</button>
                     </div>
                     </div>
                 </div>
--- src/components/SugerenciasModalComponent.jsx
+++ src/components/SugerenciasModalComponent.jsx
@@ -7,15 +7,39 @@ const SugerenciasModal = () => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                     <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="sugerenciasModalLabel">Modal title</h1>
+                        <h1 className="modal-title fs-5" id="sugerenciasModalLabel">Sugerencias</h1>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        ...
+                        <form onSubmit="" className="mt-3">
+                            <div className="mb-3">
+                                <label htmlFor="name" className="form-label" style={{ color: 'var(--color2)' }}>Nombre:</label>
+                                <input type="text" className="form-control" id="name" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label" style={{ color: 'var(--color2)' }}>Correo Electrónico:</label>
+                                <input type="email" className="form-control" id="email" />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="suggestionType" className="form-label" style={{ color: 'var(--color2)' }}>Tipo de Sugerencia:</label>
+                                <select className="form-select" id="suggestionType" >
+                                    <option value="">Selecciona una opción</option>
+                                    <option value="service">Servicio</option>
+                                    <option value="menu">Menú</option>
+                                    <option value="ambience">Ambiente</option>
+                                    <option value="other">Otro</option>
+                                </select>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="suggestion" className="form-label" style={{ color: 'var(--color2)' }}>Sugerencia:</label>
+                                <textarea className="form-control" id="suggestion" rows="3" ></textarea>
+                            </div>
+                        </form>
+                        <p>Le agradecemos la sugerencia. Trabajaremos en ella</p>
                     </div>
                     <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
+                        <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Enviar</button>
                     </div>
                     </div>
                 </div>
