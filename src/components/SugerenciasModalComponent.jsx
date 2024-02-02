4af1e722a29c4dc3c0757f0120e9985cf1f55dbd
Jorge Rea <danireaprado@hotmail.com>
2024-02-01 19:41:21 -0600

    Proyecto finalizado

--- src/components/SugerenciasModalComponent.jsx
+++ src/components/SugerenciasModalComponent.jsx
@@ -1,5 +1,39 @@
+import { useState, useEffect } from "react"
+import { crearSugerencia } from "../backend/dbOperations"
+
 
 const SugerenciasModal = () => {
+
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [tipoSugerencia, setTipoSugerencia] = useState('');
+    const [sugerencia, setSugerencia] = useState('');
+
+    const creaSugerencia = async (e) => {
+        e.preventDefault();
+
+        const nuevaSugerencia = {
+            nombre,
+            email,
+            tipo: tipoSugerencia,
+            sugerencia,
+        };
+
+        try {
+            await crearSugerencia(nuevaSugerencia); // Envía la sugerencia a Firestore
+            alert('Sugerencia enviada con éxito. ¡Gracias por tu aporte!');
+            // Limpiar el formulario
+            setNombre('');
+            setEmail('');
+            setTipoSugerencia('');
+            setSugerencia('');
+        } catch (error) {
+            console.error('Error al enviar la sugerencia:', error);
+            alert('Hubo un error al enviar tu sugerencia. Por favor, inténtalo de nuevo más tarde.');
+        }
+    };
+
+
     return (
         <>
             {/* Modal */}
@@ -11,35 +45,41 @@ const SugerenciasModal = () => {
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        <form onSubmit="" className="mt-3">
-                            <div className="mb-3">
-                                <label htmlFor="name" className="form-label" style={{ color: 'var(--color2)' }}>Nombre:</label>
-                                <input type="text" className="form-control" id="name" />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="email" className="form-label" style={{ color: 'var(--color2)' }}>Correo Electrónico:</label>
-                                <input type="email" className="form-control" id="email" />
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="suggestionType" className="form-label" style={{ color: 'var(--color2)' }}>Tipo de Sugerencia:</label>
-                                <select className="form-select" id="suggestionType" >
-                                    <option value="">Selecciona una opción</option>
-                                    <option value="service">Servicio</option>
-                                    <option value="menu">Menú</option>
-                                    <option value="ambience">Ambiente</option>
-                                    <option value="other">Otro</option>
-                                </select>
-                            </div>
-                            <div className="mb-3">
-                                <label htmlFor="suggestion" className="form-label" style={{ color: 'var(--color2)' }}>Sugerencia:</label>
-                                <textarea className="form-control" id="suggestion" rows="3" ></textarea>
-                            </div>
-                        </form>
+                    <form onSubmit={creaSugerencia} className="mt-3">
+                                {/* Campos del formulario */}
+                                {/* Nombre */}
+                                <div className="mb-3">
+                                    <label htmlFor="name" className="form-label" style={{ color: 'var(--color2)' }}>Nombre:</label>
+                                    <input type="text" className="form-control" id="name" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                                </div>
+                                {/* Email */}
+                                <div className="mb-3">
+                                    <label htmlFor="email" className="form-label" style={{ color: 'var(--color2)' }}>Correo Electrónico:</label>
+                                    <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                </div>
+                                {/* Tipo de Sugerencia */}
+                                <div className="mb-3">
+                                    <label htmlFor="suggestionType" className="form-label" style={{ color: 'var(--color2)' }}>Tipo de Sugerencia:</label>
+                                    <select className="form-select" id="suggestionType" value={tipoSugerencia} onChange={(e) => setTipoSugerencia(e.target.value)}>
+                                        <option value="">Selecciona una opción</option>
+                                        <option value="service">Servicio</option>
+                                        <option value="menu">Menú</option>
+                                        <option value="ambience">Ambiente</option>
+                                        <option value="other">Otro</option>
+                                    </select>
+                                </div>
+                                {/* Sugerencia */}
+                                <div className="mb-3">
+                                    <label htmlFor="suggestion" className="form-label" style={{ color: 'var(--color2)' }}>Sugerencia:</label>
+                                    <textarea className="form-control" id="suggestion" rows="3" value={sugerencia} onChange={(e) => setSugerencia(e.target.value)}></textarea>
+                                </div>
+                                {/* Botón de envío dentro del modal-footer */}
+                            </form>
                         <p>Le agradecemos la sugerencia. Trabajaremos en ella</p>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                        <button type="button" className="btn btn-primary" data-bs-dismiss="modal">Enviar</button>
+                        <button type="submit" className="btn btn-primary" onClick={creaSugerencia} data-bs-dismiss="modal">Enviar</button>
                     </div>
                     </div>
                 </div>
--- src/pages/RealizarPedidoPage.jsx
+++ src/pages/RealizarPedidoPage.jsx
@@ -1,6 +1,47 @@
 import "../css/realizarPedidoPageStyles.css"
+import { useEffect, useState } from "react"
+import { crearPedido } from "../backend/dbOperations"
 
 const RealizarPedidoPage = () => {
+    const [platilloSeleccionado, setPlatilloSeleccionado] = useState("");
+    const [bebidaSeleccionada, setBebidaSeleccionada] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [email, setEmail] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [horaRecogida, setHoraRecogida] = useState("");
+
+    const manejarEnvioPedido = async (e) => {
+        e.preventDefault();
+
+        const pedido = {
+            platillo: platilloSeleccionado,
+            bebida: bebidaSeleccionada,
+            nombre,
+            email,
+            telefono,
+            horaRecogida,
+        };
+
+        try {
+            await crearPedido(pedido);
+            alert("Pedido añadido con éxito");
+
+            // Reiniciar el formulario
+            setPlatilloSeleccionado("");
+            setBebidaSeleccionada("");
+            setNombre("");
+            setEmail("");
+            setTelefono("");
+            setHoraRecogida("");
+            
+        } catch (error) {
+            console.error("Error al añadir el pedido:", error);
+            alert("Error al añadir el pedido");
+        }
+    };
+
+
+
     return (
         <>
             <h1 className="text-center" style={{ color: 'var(--color3)' }}>Pide en línea</h1>
@@ -28,29 +69,77 @@ const RealizarPedidoPage = () => {
                     <h4 style={{ color: 'var(--color3)'}}>Realiza tu Pedido Aquí</h4>
                 </div>
             <div className="row-col">
-                <form className="mt-3">
+                <form className="mt-3" onSubmit={manejarEnvioPedido}>
                     {/* Detalles del Pedido */}
                     <div className="mb-3 text-center">
-                        <label htmlFor="itemName" className="form-label textoPlatillo" style={{ color: 'var(--color2)' }}>Platillo</label>
-                        <input type="text" className="form-control" id="itemName" placeholder="Nombre del platillo" />
-                        <button type="button" className="btn btn-sm mt-2 text-center" style={{ backgroundColor: 'var(--color3)', color: 'var(--color1)' }}>Añadir Platillo</button>
+                        <label htmlFor="platilloName" className="form-label textoPlatillo" style={{ color: 'var(--color2)' }}>Platillo</label>
+                        <select className="form-select" id="platilloName"  value={platilloSeleccionado} onChange={(e) => setPlatilloSeleccionado(e.target.value)} aria-label="Selecciona un platillo">
+                            <option value="">Selecciona un platillo</option>
+                            
+                            <optgroup label="Entradas">
+                                <option value="Carpaccio de Res">Carpaccio de Res - $180 MXN</option>
+                                <option value="Ensalada César">Ensalada César - $150 MXN</option>
+                                <option value="Provoleta">Provoleta - $160 MXN</option>
+                                <option value="Empanadas de Carne">Empanadas de Carne - $130 MXN c/u</option>
+                            </optgroup>
+
+                            <optgroup label="Platos Principales">
+                                <option value="Milanesa Napolitana">Milanesa Napolitana - $220 MXN</option>
+                                <option value="Risotto de Setas">Risotto de Setas - $200 MXN</option>
+                                <option value="Pasta al Pesto con Camarones">Pasta al Pesto con Camarones - $240 MXN</option>
+                                <option value="Pollo al Limón">Pollo al Limón - $190 MXN</option>
+                            </optgroup>
+
+                            <optgroup label="Cortes">
+                                <option value="Ribeye">Ribeye - $350 MXN</option>
+                                <option value="T-Bone">T-Bone - $400 MXN</option>
+                                <option value="Picanha">Picanha - $320 MXN</option>
+                                <option value="New York Strip">New York Strip - $330 MXN</option>
+                            </optgroup>
+
+                            <optgroup label="Tacos">
+                                <option value="Tacos de Arrachera">Tacos de Arrachera - $150 MXN por orden</option>
+                                <option value="Tacos al Pastor">Tacos al Pastor - $130 MXN por orden</option>
+                                <option value="Tacos de Pescado Estilo Baja">Tacos de Pescado Estilo Baja - $160 MXN por orden</option>
+                                <option value="Tacos de Camarón al Ajillo">Tacos de Camarón al Ajillo - $180 MXN por orden</option>
+                            </optgroup>
+                        </select>
+                    </div>
+
+                    <div className="mb-3 text-center">
+                        <label htmlFor="drinkName" className="form-label textoPlatillo" style={{ color: 'var(--color2)' }}>Bebida</label>
+                        <select className="form-select" id="drinkName" value={bebidaSeleccionada} onChange={(e) => setBebidaSeleccionada(e.target.value)} aria-label="Default select example">
+                            <option value="">Selecciona una bebida</option>
+
+                            <optgroup label="Bebidas Alcohólicas">
+                                <option value="Margarita Clásica">$120 MXN - Margarita Clásica</option>
+                                <option value="Mojito">$110 MXN - Mojito</option>
+                                <option value="Sangría">$90 MXN - Sangría</option>
+                                <option value="Cerveza Artesanal">$70 MXN - Cerveza Artesanal</option>
+                                <option value="Vino">$100 MXN - Vino</option>
+                            </optgroup>
+
+                            <optgroup label="Bebidas No Alcohólicas">
+                                <option value="Agua de Horchata">$50 MXN - Agua de Horchata</option>
+                                <option value="Limonada Fresca">$45 MXN - Limonada Fresca</option>
+                                <option value="Té Helado">$40 MXN - Té Helado</option>
+                                <option value="Refrescos y Aguas Gaseosas">$35 MXN - Refrescos y Aguas Gaseosas</option>
+                                <option value="Smoothies de Frutas">$60 MXN - Smoothies de Frutas</option>
+                            </optgroup>
+                        </select>
                     </div>
                     
                     <div className="mb-3">
-                        <label htmlFor="name" className="form-label" style={{ color: 'var(--color2)' }}>Nombre</label>
-                        <input type="text" className="form-control" id="name" placeholder="Tu nombre" />
+                    <input type="text" className="form-control" id="namePedido" value={nombre} onChange={(e) => setNombre(e.target.value)} placeholder="Tu nombre" />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="email" className="form-label" style={{ color: 'var(--color2)' }}>Correo eléctronico</label>
-                        <input type="email" className="form-control" id="phone" placeholder="Tu correo electrónico" />
+                        <input type="email" className="form-control" id="emailPedido" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Tu correo electrónico" />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="phone" className="form-label" style={{ color: 'var(--color2)' }}>Teléfono</label>
-                        <input type="tel" className="form-control" id="phone" placeholder="Tu teléfono" />
+                        <input type="tel" className="form-control" id="phonePedido" value={telefono} onChange={(e) => setTelefono(e.target.value)} placeholder="Tu teléfono" />
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="pickupTime" className="form-label" style={{ color: 'var(--color2)' }}>Hora de Recogida</label>
-                        <input type="time" className="form-control" id="pickupTime" />
+                        <input type="time" className="form-control" id="pickupTime" value={horaRecogida} onChange={(e) => setHoraRecogida(e.target.value)} />
                     </div>
                     
                     {/* Botón de Envío */}
